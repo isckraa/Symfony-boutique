b3e0866f0c5eec916e4d34436317b0c365dd1646
nelu <wf.nelu@yandex.ru>
2020-03-19 00:19:26 +0100

    panier-v2

--- e-boutique/src/Controller/PanierController.php
+++ e-boutique/src/Controller/PanierController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PanierController extends AbstractController
@@ -11,19 +12,39 @@ class PanierController extends AbstractController
     /**
      * @Route("/panier", name="panier")
      */
-    public function index()
+    public function index( SessionInterface $session, ProduitRepository $productRepository )
     {
+        $panier = $session->get( 'panier', [] );
+
+        $panierWithData = [];
+
+        foreach( $panier as $id => $quantity ) {
+            $panierWithData[] = [
+                'product'   => $productRepository->find( $id ),
+                'quantity'  => $quantity
+            ];
+        }
+
+        $total = 0;
+
+        foreach( $panierWithData as $item ) {
+            $totalItem = $item['product']->getPrix() * $item['quantity'];
+            $total += $totalItem;
+        }
+
+
         return $this->render( 'panier/index.html.twig', [
             'controller_name' => 'PanierController',
+            'items'           => $panierWithData,
+            'total'           => $total
         ]);
     }
 
     /**
      * @Route("/panier/add/{id}", name="produit_add")
      */
-    public function add( $id, Request $request )
+    public function add( $id, SessionInterface $session )
     {
-        $session = $request->getSession();
         $panier = $session->get( 'panier', [] );
 
         if( !empty( $panier[$id] ) ) {
@@ -32,9 +53,23 @@ class PanierController extends AbstractController
             $panier[$id] = 1;
         }
 
+        $session->set( 'panier', $panier );
+
+        return $this->redirectToRoute( "panier" );
+    }
+
+    /**
+     * @Route("/panier/supprimer/{id}", name="panier_remove")
+     */
+    public function remove( $id, SessionInterface $session ) {
+        $panier = $session->get( 'panier', [] );
+
+        if( !empty( $panier ) ){
+            unset( $panier[$id] );
+        }
 
         $session->set( 'panier', $panier );
 
-        dd( $session->get( 'panier' ) );
+        return $this->redirectToRoute( "panier" );
     }
 }
