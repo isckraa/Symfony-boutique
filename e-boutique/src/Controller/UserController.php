8e23abbcfd272a1f3b10de7c644a65339afe9f3a
nelu <wf.nelu@yandex.ru>
2020-04-05 13:21:13 +0200

    user pasword modification

--- e-boutique/src/Controller/UserController.php
+++ e-boutique/src/Controller/UserController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/user")
@@ -20,13 +21,19 @@ class UserController extends AbstractController
      */
     public function index(UserRepository $userRepository): Response
     {
-        $userEmail = $this->getUser()->getEmail();
-        $userId = $this->getUser()->getId();
+        if( $this->getUser() !== null ) {
+            $userEmail = $this->getUser()->getEmail();
+            $userId = $this->getUser()->getId();
 
-        return $this->render('user/index.html.twig', [
-            'user' => $userRepository->findByEmail( $userEmail ),
-            'userId' => $userId,
-        ]);
+            return $this->render('user/index.html.twig', [
+                'user' => $userRepository->findByEmail( $userEmail ),
+                'userId' => $userId,
+            ]);
+        } else {
+            return $this->redirectToRoute( 'home' );
+        }
+
+
     }
 
     /**
@@ -65,12 +72,20 @@ class UserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, User $user): Response
+    public function edit(Request $request, UserPasswordEncoderInterface $passwordEncoder, User $user): Response
     {
+        
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
-
+        
         if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('user_index');
